4302a50444ea2988c7ef935a6845a3b35cae929c
monisax92 <monisax92@gmail.com>
2022-11-08 18:14:58 +0100

    first version of website still with few placeholders

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,18 @@
+import "../styles/styles.css";
+import lazySizes from "lazysizes";
+import MobileMenu from "./modules/MobileMenu";
+import StickyHeader from "./modules/StickyHeader";
+import RevealOnScroll from "./modules/RevealOnScroll";
+import "./modules/accordion";
+
+new RevealOnScroll(document.querySelectorAll(".feature-card"), 75);
+new RevealOnScroll(document.querySelectorAll(".service-card"), 85);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 85);
+new RevealOnScroll(document.querySelectorAll(".gallery__photo"), 75);
+
+let stickyHeader = new StickyHeader();
+let mobileMenu = new MobileMenu();
+
+if (module.hot) {
+  module.hot.accept();
+}
